a9bd2e40c3b08315f2a7efee850444ed1297ff3c
ogorodnikoff2012 <ogorodnikoff2012@yandex.ru>
2017-12-02 15:46:10 +0300

    Detailed info about service

--- main.py
+++ main.py
@@ -16,6 +16,13 @@ def chunks(lst, n):
         yield lst[i:i + n]
 
 
+def query_type(query):
+    return query.split('@')[0]
+
+def parse_args(query):
+    return query.split('@')[1:]
+
+
 def print_services(chat_id, services):
     def counter():
         counter.cnt += 1
@@ -29,7 +36,7 @@ def print_services(chat_id, services):
         buttons = []
         for service in service_chunk:
             button = types.InlineKeyboardButton('Подробнее: ' + service['name'],
-                                                callback_data="detailed_service@" + service['slug'])
+                                                callback_data='detailed_service@' + service['slug'])
             buttons.append(button)
         keyboard.add(*buttons)
         bot.send_message(chat_id, msg, reply_markup=keyboard, parse_mode='Markdown')
@@ -42,6 +49,27 @@ def get_service_list(message):
         bot.send_message(message.chat.id, words.REQUEST_FAILED)
     else:
         print_services(message.chat.id, services)
+
+
+def print_detailed_service(chat_id, service):
+    msg = '*' + service['name'] + '*\n' + service['description'] + '\n' + words.ARCHIVE + ': ' +\
+          words.yesno(service['archive']) + '\n' + words.CONN_FEE + ': ' + str(service['connectionFee']) + '\n' +\
+          words.SUBSCR_FEE + ': ' + str(service['subscriptionFee'])
+    kb = types.InlineKeyboardMarkup()
+    to_site = types.InlineKeyboardButton(text='Подробнее на сайте', url=service['url'])
+    add_service = types.InlineKeyboardButton(text='Подключить', callback_data='add_service@' + service['slug'])
+    kb.add(to_site, add_service)
+    bot.send_message(chat_id, msg, reply_markup=kb, parse_mode='Markdown')
+
+
+@bot.callback_query_handler(lambda x: query_type(x.data) == 'detailed_service')
+def callback_detailed_service(call):
+    args = parse_args(call.data)
+    service = api.services.get_info(args[0])
+    if service.status_code != 200:
+        bot.send_message(call.message.chat.id, words.REQUEST_FAILED)
+    else:
+        print_detailed_service(call.message.chat.id, service.body['data'])
 
 
 @bot.message_handler(commands=['help', 'start'])
--- words.py
+++ words.py
@@ -1,1 +1,7 @@
 REQUEST_FAILED = "Ой, чёт не зашло..."
+ARCHIVE = "Архивный"
+CONN_FEE = "Стоимость подключения"
+SUBSCR_FEE = "Стоимость использования"
+
+def yesno(flag):
+    return 'Да' if flag else 'Нет'
