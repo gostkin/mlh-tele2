9c63be32c2dcf21913c5994a939f0bac6d923863
ogorodnikoff2012 <ogorodnikoff2012@yandex.ru>
2017-12-02 13:43:07 +0300

    Test some functions

--- main.py
+++ main.py
@@ -2,14 +2,30 @@
 
 import config
 import telebot
+import rest
+import words
 
 bot = telebot.TeleBot(config.token)
+api = rest.build_tele2_api()
 
 
-@bot.message_handler(content_types=['text'])
+#@bot.message_handler(content_types=['text'])
 def repeat_all_messages(message):
     bot.send_message(message.chat.id, message.text)
     print(message)
+
+def print_service(chat_id, service):
+    message = '*' + service['name'] + '*\n' + service['description']
+    bot.send_message(chat_id, message, parse_mode='Markdown')
+
+@bot.message_handler(commands=['getServices'])
+def get_service_list(message):
+    services = api.services.get_available_services()
+    if services.status_code != 200:
+        bot.send_message(message.chat.id, words.REQUEST_FAILED)
+    else:
+        for service in services.body['data']:
+            print_service(message.chat.id, service)
 
 
 if __name__ == '__main__':
