c87d90becf5fec6f02a8b0a51f780e19397ea01b
ogorodnikoff2012 <ogorodnikoff2012@yandex.ru>
2017-12-02 15:07:09 +0300

    Service list - buttons

--- main.py
+++ main.py
@@ -11,7 +11,12 @@ bot = telebot.TeleBot(config.token)
 api = rest.build_tele2_api()
 
 
-@bot.message_handler(content_types=["text"])
+def chunks(lst, n):
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
+@bot.message_handler(func=lambda x: False, content_types=["text"])
 def default_test(message):
     keyboard = types.InlineKeyboardMarkup()
     url_button = types.InlineKeyboardButton(text="Хуи дрочены", url="https://ya.ru")
@@ -19,23 +24,27 @@ def default_test(message):
     keyboard.row(url_button, url1)
     bot.send_message(message.chat.id, "Привет! Нажми на кнопку и перейди в поисковик.", reply_markup=keyboard)
 
-#@bot.message_handler(content_types=['text'])
-def repeat_all_messages(message):
-    bot.send_message(message.chat.id, message.text)
-    print(message)
-
 
 def print_services(chat_id, services):
-    ret = ""
-    iter = 1
-    for service in services.body['data']:
-        ret += '*' + str(iter) + '. ' + service['name'] + '*\n' + service['description'] + '\n\n'
-        iter += 1
+    def counter():
+        counter.cnt += 1
+        return counter.cnt
+    counter.cnt = 0
 
-    bot.send_message(chat_id, ret, parse_mode='Markdown')
+    for service_chunk in chunks(services.body['data'], 2):
+        msg = '\n\n'.join(map(lambda service: '*' + str(counter()) + '. ' + service['name'] +
+                                              '*\n' + service['description'], service_chunk))
+        keyboard = types.InlineKeyboardMarkup(row_width=2)
+        buttons = []
+        for service in service_chunk:
+            button = types.InlineKeyboardButton('Подробнее: ' + service['name'],
+                                                callback_data="detailed_service@" + service['slug'])
+            buttons.append(button)
+        keyboard.add(*buttons)
+        bot.send_message(chat_id, msg, reply_markup=keyboard, parse_mode='Markdown')
 
 
-@bot.message_handler(commands=['get_services'])
+@bot.message_handler(func=lambda x: True, commands=['services'])
 def get_service_list(message):
     services = api.services.get_available_services()
     if services.status_code != 200:
@@ -43,6 +52,12 @@ def get_service_list(message):
     else:
         print_services(message.chat.id, services)
 
+
+@bot.message_handler(commands=['help', 'start'])
+def get_help(message):
+    bot.send_message(message.chat.id, """Список команд:
+/help - вывести справку
+/services - получить список сервисов""")
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
